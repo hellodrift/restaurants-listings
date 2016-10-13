c0b08b7a32a41d37b2409713b45f204141ac0ebd
Kishore <babunsahoo@gmail.com>
2016-10-13 21:07:28 +0530

    code refractor job to resturants

---
+++ templates/restaurant-application-url.php
@@ -0,0 +1,1 @@
+<p><?php _e( 'To apply for this job please visit the following URL:', 'listings-jobs' ); ?> <a href="<?php echo esc_url( $apply->url ); ?>" target="_blank" rel="nofollow"><?php echo esc_html( $apply->url ); ?> &rarr;</a></p>
